e9ad2a3908731d5d1b8abf69f5a640a870498953
Sufyane Ramseyn <sufyanemoufoutaou819@gmail.com>
2023-07-11 17:30:41 +0200

    depot react-laravel initial

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,11 @@
+import React,{useState} from 'react'
+
+const Test = (props) => {
+  return (
+    <div>
+      <h1 className=' text-2xl font-bold'> This is test component {props.test} </h1>
+    </div>
+  )
+}
+
+export default Test
